4df2ade3eba48e4e513a7869863087cb20ec8b39
Utku Demir <me@utdemir.com>
2017-11-08 13:56:39 +1300

    Remove duplicate luks configuration
    Which was causing duplicate password prompts on boot

--- galactica-configuration.nix
+++ galactica-configuration.nix
@@ -8,17 +8,28 @@
   boot.loader.grub.version = 2;
   boot.loader.grub.device = "/dev/sda";
 
-  boot.initrd.luks.devices = [
-    {
-      name = "root";
-      device = "/dev/disk/by-uuid/fd770830-0e38-4125-88e1-7efec57e9e5c";
-      allowDiscards = true;
-    }
-  ];
-
   swapDevices = [
     { device = "/swapfile"; size = 4096; }
   ];
 
   networking.hostName = "galactica";
+
+  networking.extraHosts = ''
+    127.0.0.1 galactica
+    10.6.210.56     ip-10-6-210-56.mm.movio.co
+    10.6.210.148    ip-10-6-210-148.mm.movio.co
+    10.6.210.127    ip-10-6-210-127.mm.movio.co
+    10.6.210.239    ip-10-6-210-239.mm.movio.co
+    10.6.210.226    ip-10-6-210-226.mm.movio.co
+    10.6.200.122    ip-10-6-200-122.mm.movio.co
+    10.6.200.190    ip-10-6-200-190.mm.movio.co
+    10.6.200.140    ip-10-6-200-140.mm.movio.co
+    10.6.200.133    ip-10-6-200-133.mm.movio.co
+    10.6.200.107    ip-10-6-200-107.mm.movio.co
+    10.6.210.243    ip-10-6-210-243.mm.movio.co
+    10.6.210.156    ip-10-6-210-156.mm.movio.co
+    10.6.210.206    ip-10-6-210-206.mm.movio.co
+    10.6.210.224    ip-10-6-210-224.mm.movio.co
+    10.6.210.85     ip-10-6-210-85.mm.movio.co
+  '';
 }
--- serenity-configuration.nix
+++ serenity-configuration.nix
@@ -8,14 +8,6 @@
   boot.loader.grub.version = 2;
   boot.loader.grub.device = "/dev/sda";
 
-  boot.initrd.luks.devices = [
-    {
-      name = "root";
-      device = "/dev/disk/by-uuid/5eff54ab-ff36-4e43-a158-0cd54234b7c0";
-      allowDiscards = true;
-    }
-  ];
-
   swapDevices = [
     { device = "/swapfile"; size = 2048; }
   ];
