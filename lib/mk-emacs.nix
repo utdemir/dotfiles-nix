1020883501c672c44c2333182e0b3d402a6c0797
Utku Demir <me@utdemir.com>
2018-04-21 20:10:24 +1200

    Use emacs25

--- lib/mk-emacs.nix
+++ lib/mk-emacs.nix
@@ -2,10 +2,10 @@
 
 let
   packages = import (pkgs.runCommand "mk-emacs" {
-    buildInputs = [ (pkgs.emacs26WithPackages (epkgs: [])) ];
+    buildInputs = [ (pkgs.emacsWithPackages (epkgs: [])) ];
   } "emacs --script ${./use-package-to-nix.el} ${el} > $out");
 in
-  pkgs.emacs26WithPackages (epkgs:
-    let s = epkgs.melpaPackages // epkgs.elpaPackages;
+  pkgs.emacsWithPackages (epkgs:
+    let s = epkgs.melpaPackages;
     in  builtins.map (i: s.${i}) (packages ++ ["use-package"])
   )
--- pkgs.nix
+++ pkgs.nix
@@ -1,10 +1,9 @@
 let
-#nixpkgs = (import <nixpkgs> {}).fetchFromGitHub {
-#  owner = "NixOS";
-#  repo = "nixpkgs";
-#  rev = "ad78e52357eb3df0f457c40185504567c7da524e";
-#  sha256 = "1gxynidpk23w7mabad5y35cmyhx1jsdfxl8r2xn4d0gnv39d090y";
-#};
-nixpkgs = ../nixpkgs;
+nixpkgs = (import <nixpkgs> {}).fetchFromGitHub {
+  owner = "NixOS";
+  repo = "nixpkgs";
+  rev = "ad78e52357eb3df0f457c40185504567c7da524e";
+  sha256 = "1gxynidpk23w7mabad5y35cmyhx1jsdfxl8r2xn4d0gnv39d090y";
+};
 in
 import nixpkgs { config.allowUnfree = true; }
