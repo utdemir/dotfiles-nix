e25972ef171280ab5b17cac19486e6327ecc0fe0
Utku Demir <me@utdemir.com>
2018-05-07 17:02:22 +1200

    Start using org-mode

--- dotfiles/emacs.el
+++ dotfiles/emacs.el
@@ -186,6 +186,14 @@
   :config
   (dumb-jump-mode))
 
+(with-eval-after-load 'org
+  (define-key org-mode-map (kbd "<S-left>") nil)
+  (define-key org-mode-map (kbd "<S-right>") nil)
+  (define-key org-mode-map (kbd "<S-up>") nil)
+  (define-key org-mode-map (kbd "<S-down>") nil)
+  (define-key org-mode-map (kbd "<M-left>") nil)
+  (define-key org-mode-map (kbd "<M-right>") nil))
+
 ;; Custom
 
 (defun yank-to-x-clipboard ()
