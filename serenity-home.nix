6934a0b0763a9eef6aed84d20d7d88f0442218a1
Utku Demir <me@utdemir.com>
2018-07-19 19:16:31 +1200

    serenity: Remove minidlna and dropbox

--- serenity-home.nix
+++ serenity-home.nix
@@ -7,15 +7,9 @@ in
   imports = [ ./common-home.nix ];
 
   home.packages = with pkgs; [
-    minidlna deluge dropbox
+    deluge
 
     steam
     google-cloud-sdk
   ];
-
-  home.file.".minidlna.conf".text = ''
-    friendly_name=SerenityEntertainmentConsole
-    media_dir=/home/utdemir/Downloads
-    db_dir=/home/utdemir/.cache/minidlna
-    '';
 }
