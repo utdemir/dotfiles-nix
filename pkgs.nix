4c4cda0dffbd767bc7ca4c2dc6384f41496cc675
Utku Demir <me@utdemir.com>
2018-07-19 12:24:39 +1200

    Update nixpkgs

--- pkgs.nix
+++ pkgs.nix
@@ -2,8 +2,8 @@ let
 nixpkgs = (import <nixpkgs> {}).fetchFromGitHub {
   owner = "NixOS";
   repo = "nixpkgs";
-  rev = "394ef8245153fceedf21928f6175ff8bc691b71b";
-  sha256 = "0zmkrwzbbv1kf4f56pn20g1yyn6hih7w4ibdzv6gak783scrsbnb";
+  rev = "4c58c20efccc663929e70d138f49cdad0e7537e7";
+  sha256 = "0ynd9bg5n5c2fsvbv57dj6hwwv9pcibzpnlg3dsbxd1v8p3kv1qz";
 };
 in
 import nixpkgs { config.allowUnfree = true; }
