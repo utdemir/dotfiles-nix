92d1d72754826703f553be304059f102c7b51806
Utku Demir <me@utdemir.com>
2019-05-12 06:56:42 +1200

    Temporarily disable xautolock

--- dotfiles/i3/autostart.sh
+++ dotfiles/i3/autostart.sh
@@ -7,6 +7,6 @@ unclutter &
 nm-applet &
 parcellite &
 pasystray &
-xautolock -locker "i3lock --color '#332233'"  -time 5 -detectsleep &
+# xautolock -locker "i3lock --color '#332233'"  -time 5 -detectsleep &
 redshift -l -36.84853:174.76349 & # auckland, nz
 
