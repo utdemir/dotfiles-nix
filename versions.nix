bda4a268443c57eabf9bb21e0794536022d0cc88
Utku Demir <me@utdemir.com>
2019-07-03 14:35:26 +1200

    Update versions

--- versions.nix
+++ versions.nix
@@ -1,10 +1,10 @@
 {
   home-manager = builtins.fetchGit {
     url = "https://github.com/rycee/home-manager";
-    rev = "8f7cd532040d14e3db907c6ffaa4a149e443d5e4";
+    rev = "28f2dd612ec7c3bd07ec951aa6862d0702ab6624";
   };
   nixpkgs = builtins.fetchGit {
     url = "https://github.com/NixOS/nixpkgs";
-    rev = "a2075a2c314ccb3df6522dd957bb4e237446dc49";
+    rev = "ed952d43b9caaf7563a615166d4b83d451f91c86";
   };
 }
