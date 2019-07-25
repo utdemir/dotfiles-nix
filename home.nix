1b13badfc01988ddafd6f3b6cf8ce2e5e316781a
Utku Demir <me@utdemir.com>
2019-07-25 23:04:37 +1200

    Add git to lorri's PATH

--- home.nix
+++ home.nix
@@ -156,7 +156,7 @@ in
       ProtectSystem = "strict";
       WorkingDirectory = "%h";
       Restart = "on-failure";
-      Environment = "PATH=${pkgs.nix}/bin RUST_BACKTRACE=1";
+      Environment = "PATH=${pkgs.nix}/bin:${pkgs.git}/bin RUST_BACKTRACE=1";
     };
   };
   
