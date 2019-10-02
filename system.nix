2b6f9e31a23eca5dde13e8b2f5c023fde26711ef
Utku Demir <me@utdemir.com>
2019-10-02 16:53:39 +1300

    Correctly import system-private.nix

--- system.nix
+++ system.nix
@@ -11,7 +11,7 @@ let
 in
 {
   imports =
-    builtins.filter builtins.pathExists [ ./home-private.nix ];
+    builtins.filter builtins.pathExists [ ./system-private.nix ];
 
   networking.hostName = user.hostname;
 
