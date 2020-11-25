274d963e206d1e86e0812fbb2958ed0b3eff2039
utdemir <me@utdemir.com>
2020-11-26 09:59:38 +1300

    Update battery-notification.sh to be more explicit

--- scripts/battery-notification.sh
+++ scripts/battery-notification.sh
@@ -3,9 +3,14 @@
 LOW_THRESHOLD=10
 CRIT_THRESHOLD=3
 
-perc="$(acpi -b | grep 'Battery 0' | grep 'Discharging' | grep -Po '[0-9]+(?=%)')"
+is_charging=$(cat /sys/class/power_supply/AC/online)
 
-if [[ -z "$perc" ]]; then exit 0
+energy_full=$(cat /sys/class/power_supply/BAT*/energy_full | awk '{s+=$1} END {print s}'   )
+energy_now=$(cat /sys/class/power_supply/BAT*/energy_now | awk '{s+=$1} END {print s}'   )
+
+perc=$(( energy_now * 100 / energy_full ))
+
+if [[ -z "$perc" ]]; then exit 1
 elif [[ "$perc" -lt "$CRIT_THRESHOLD" ]]; then notify-send -u critical "Battery is critical! ($perc%)"
 elif [[ "$perc" -lt "$LOW_THRESHOLD" ]]; then notify-send -u normal "Battery is low! ($perc%)"
 fi
