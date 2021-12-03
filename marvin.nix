397ab51efc13e5e227d333b967184853dd1fcec4
Utku Demir <me@utdemir.com>
2021-12-04 11:48:21 +1300

    Enable memtest86 on marvin

--- marvin.nix
+++ marvin.nix
@@ -12,7 +12,11 @@
     # HARDWARE #
     ############
 
-    boot.loader.systemd-boot.enable = true;
+    boot.loader.systemd-boot = {
+      enable = true;
+      memtest86.enable = true;
+    };
+
     boot.loader.efi.canTouchEfiVariables = true;
 
     hardware.cpu.amd.updateMicrocode = true;
